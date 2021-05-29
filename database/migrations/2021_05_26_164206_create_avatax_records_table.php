e940b508b55c52795ae0ce02e3816fd8257b1d9a
smile <ywjmylove@163.com>
2021-05-29 10:36:05 +0800

    更改成单值索引

--- database/migrations/2021_05_26_164206_create_avatax_records_table.php
+++ database/migrations/2021_05_26_164206_create_avatax_records_table.php
@@ -13,19 +13,21 @@ class CreateAvataxRecordsTable extends Migration
      */
     public function up()
     {
-        Schema::create('avatax_records', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('user_id')->nullable(false)->comment('用户id');
-            $table->string('document_id')->nullable(false)->comment('订单id');
-            $table->string('address',500)->nullable(false)->comment('订单地址');
-            $table->string('from',500)->nullable(false)->comment('发货地址');
-            $table->text('order')->nullable(false)->comment('订单信息');
-            $table->unsignedTinyInteger('status')->nullable(false)->default(0)->comment('状态 默认为0 0表示失败 1表示成功');
-            $table->text('response')->nullable(false)->comment('接口响应数据');
-            $table->timestamp('created_at')->nullable(false)->comment('请求时间');
+        if (!Schema::hasTable('avatax_records')) {
+            Schema::create('avatax_records', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('user_id')->nullable(false)->comment('用户id');
+                $table->string('document_id')->nullable(false)->comment('订单id');
+                $table->string('address', 500)->nullable(false)->comment('订单地址');
+                $table->string('from', 500)->nullable(false)->comment('发货地址');
+                $table->text('order')->nullable(false)->comment('订单信息');
+                $table->unsignedTinyInteger('status')->nullable(false)->default(0)->comment('状态 默认为0 0表示失败 1表示成功');
+                $table->text('response')->nullable(false)->comment('接口响应数据');
+                $table->timestamp('created_at')->nullable(false)->comment('请求时间');
 
-            $table->index(['user_id','document_id'],'index_user_id_document_id');
-        });
+                $table->index(['document_id'], 'index_document_id');
+            });
+        }
     }
 
     /**
