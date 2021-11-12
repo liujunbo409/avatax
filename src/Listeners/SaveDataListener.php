66365e6e041d41d92bf78855a4dbd014ff002047
smile <ywjmylove@163.com>
2021-11-12 11:26:30 +0800

    添加日志记录

--- src/Listeners/SaveDataListener.php
+++ src/Listeners/SaveDataListener.php
@@ -2,6 +2,7 @@
 
 namespace Smbear\Avatax\Listeners;
 
+use Illuminate\Support\Facades\Log;
 use Smbear\Avatax\Events\SaveDataEvent;
 use Smbear\Avatax\Models\AvataxRecords;
 
@@ -9,8 +10,11 @@ class SaveDataListener
 {
     public function handle(SaveDataEvent $event)
     {
-        if ($event->type == 'SalesInvoice'){
+        try {
             AvataxRecords::create($event->data);
+        }catch (\Exception $exception){
+            Log::channel(config('avatax.channel'))
+                ->info('数据库记录异常'.json_encode($event->data).'异常原因：'.$exception->getMessage());
         }
     }
 }
